ec9267162921e2ddb8e1ed47149986743359271e
DELL <sangyeop0529@gmail.com>
2025-06-03 22:46:18 +0900

    css 적용

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -5,11 +5,11 @@ const VideoCard = ({ video }) => {
   const { channelTitle, title, thumbnails, publishedAt } = video.snippet;
   return (
     <li>
-      <img src={thumbnails.medium.url} alt={title} />
+      <img className="w-full" src={thumbnails.medium.url} alt={title} />
       <div>
-        <p>{title}</p>
-        <p>{channelTitle}</p>
-        <p>{getRelativeTime(publishedAt)}</p>
+        <p className="font-semibold my-2 line-clamp-2">{title}</p>
+        <p className="text-sm opacity-80">{channelTitle}</p>
+        <p className="text-sm opacity-80">{getRelativeTime(publishedAt)}</p>
       </div>
     </li>
   );
