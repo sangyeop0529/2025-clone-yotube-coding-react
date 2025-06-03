d41f2a1594acae975f6936606651ba644effa870
DELL <sangyeop0529@gmail.com>
2025-06-03 21:12:15 +0900

    Context 사용하기

---
+++ src/context/YoutubeApiContext.jsx
@@ -0,0 +1,16 @@
+import { createContext, useContext } from "react";
+import Youtube from "../api/youtube";
+import FakeYoutube from "../api/fakeYoutube";
+
+export const YoutubeApiContext = createContext();
+
+// const youtube = new Youtube();
+const youtube = new FakeYoutube();
+
+export function YoutubeApiProvider({ children }) {
+  return <YoutubeApiContext value={{ youtube }}>{children}</YoutubeApiContext>;
+}
+
+export function useYoutubeApi() {
+  return useContext(YoutubeApiContext);
+}
