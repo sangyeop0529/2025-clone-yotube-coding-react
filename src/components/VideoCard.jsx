141387b719f73a5fb93bef7e06f16adbed0fba96
DELL <sangyeop0529@gmail.com>
2025-06-03 23:32:20 +0900

    상세페이지

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import { getRelativeTime } from "../util/date";
+import { useNavigate } from "react-router-dom";
 
 const VideoCard = ({ video }) => {
   const { channelTitle, title, thumbnails, publishedAt } = video.snippet;
+  const navigate = useNavigate();
   return (
-    <li>
+    <li
+      onClick={() => {
+        navigate(`videos/watch/${video.id}`, { state: { video } });
+      }}>
       <img className="w-full" src={thumbnails.medium.url} alt={title} />
       <div>
         <p className="font-semibold my-2 line-clamp-2">{title}</p>
